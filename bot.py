266f063f19bf8a967c9f4a643758860b25e0421a
Denis Scherbatyuk <denvol100@gmail.com>
2019-05-25 08:08:03 +0700

    added a constellation module

--- bot.py
+++ bot.py
@@ -1,10 +1,8 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
 import settings
-
-# Настройка прокси
-PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080',
-    'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
+import ephem
+import time
 
 logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
@@ -13,7 +11,9 @@ logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
 
 # Создадим функцию greet_user
 def greet_user(bot, update): # bot - экземпляр нашего бота, с помощью него даем команды, update - сообщение от telegram
-    text = 'Starting / start'
+    text = 'Данный бот предназначен для вывода информации о текущем положении планет\
+    введите команду в формате /planet "название планеты на английском", например\
+    /planet Mars'
     print(text)
     update.message.reply_text(text)
 
@@ -23,6 +23,34 @@ def talk_to_me(bot, update):
     logging.info('User: %s, Chat id: %s, Message: %s', update.message.chat.username,
                 update.message.chat.id, update.message.text)
     update.message.reply_text(user_text)
+
+# Создадим функцию, которая показывает в каком созвездии находится планета, используя модуль ephem
+def constell_panet(bot, update):
+    planet = update.message.text.split()[-1]
+    now_date = time.strftime('%Y/%m/%d')
+    print(planet, now_date)
+    if planet == 'Mercury':
+        ep_planet = ephem.Mercury(now_date)
+    elif planet == 'Venus':
+        ep_planet = ephem.Venus(now_date)
+    elif planet == 'Mars':
+        ep_planet = ephem.Mars(now_date)
+    elif planet == 'Jupiter':
+        ep_planet = ephem.Jupiter(now_date)
+    elif planet == 'Saturn':
+        ep_planet = ephem.Saturn(now_date)
+    elif planet == 'Uranus':
+        ep_planet = ephem.Uranus(now_date)
+    elif planet == 'Neptune':
+        ep_planet = ephem.Neptune(now_date)
+    else:
+        print('Ввод неверный')
+    const = ephem.constellation(ep_planet)
+    print(const)
+    user_text_const = f'Планета {planet} сегодня находится в созвездии {const}'
+    print(user_text_const)
+    update.message.reply_text(user_text_const)
+
 
 # Тело бота, объявляем функцию main()
 def main():
@@ -35,6 +63,9 @@ def main():
     # создание объекта принимающие входящие сообщения и передающий их дальше (обработчик команд)
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler('start', greet_user)) # 'start' - команда в telegram, greet_user - название функции (любое)
+
+    # добавление команды в бота, которая принимает на вход название планеты
+    dp.add_handler(CommandHandler('planet', constell_panet))
 
     # хэндлер для перехвата текстовых сообщений пользователя
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
